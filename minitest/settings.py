88b75f96b63626061b6e799bdb53bccb9d6d1c6a
Urusaitech <100962655+Urusaitech@users.noreply.github.com>
2022-08-09 01:40:56 +0300

    Update settings.py

--- minitest/settings.py
+++ minitest/settings.py
@@ -19,9 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-mnquqkj(lm76l(szz%(!f_q)1@i^9=@hft=f$5&o6s_gmm45(5'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
